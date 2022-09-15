fe6eab0afff6da26d76f04c903bdf34bc8e90526
Syawali Putra <hafiddevils.123@gmail.com>
2022-09-15 13:15:07 +0700

    link kemarin

---
+++ database/migrations/2022_08_18_080825_create_student_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('adress');
+            $table->date('birth_date');
+            $table->integer('weight');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
